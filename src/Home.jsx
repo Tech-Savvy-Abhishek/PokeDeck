2ed5512cb87e83ec9616a32ca8dfff9543ce9875
Tech-Savvy-Abhishek <abhishekgs999@outlook.com>
2024-05-21 20:26:24 +0530

    Handle Async API calls

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,26 +5,28 @@ export default function Home()
 {
     const [pokemonList, setPokemonList] = useState([]);
 
-
     useEffect(() =>
     {
         fetch('https://pokeapi.co/api/v2/pokemon?limit=150')
             .then((res) => res.json())
-            .then((pokemons) => pokemons.results.forEach(pokemon =>
+            .then((pokemons) =>
             {
-                let url = pokemon.url;
-                fetch(url)
-                    .then((response) => response.json())
-                    .then((pokeData) => setPokemonList(prevPokemonList => [...prevPokemonList, pokeData]));
-            }));
+                const promises = pokemons.results.map(pokemon =>
+                {
+                    return fetch(pokemon.url)
+                        .then((response) => response.json());
+                });
+
+                Promise.all(promises)
+                    .then((pokeDataArray) => setPokemonList(pokeDataArray));
+            });
     }, []);
 
     return (
         <div className='w-fit mx-auto my-10 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 '>
             {pokemonList.map((pokemon, idx) => (
                 <Card image={pokemon['sprites']['other']['home']['front_default']} name={pokemon['name']} types={pokemon['types']} key={idx} />
-            )
-            )}
+            ))}
         </div>
     );
 }
